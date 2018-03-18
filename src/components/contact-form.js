ab2a53970741b2e8cccd6d2624e0756051a8b868
Chris Butera <cbutera@pch.com>
2018-03-18 17:38:55 -0400

    contact form bare bones

---
+++ src/components/contact-form.js
@@ -0,0 +1,27 @@
+import React from "react";
+
+class ContactForm extends React.Component {
+	render () {
+		return (
+			<form name="contact" method="POST" netlify>
+				<div>
+					<label htmlFor="name">Name:</label>
+					<input type="text" id="name" name="user_name" />
+				</div>
+				<div>
+					<label htmlFor="mail">E-mail:</label>
+					<input type="email" id="mail" name="user_mail" />
+				</div>
+				<div>
+					<label htmlFor="msg">Message:</label>
+					<textarea id="msg" name="user_message" />
+				</div>
+				<div className="button">
+					<button type="submit">Send your message</button>
+				</div>
+			</form>
+		);
+	}
+}
+
+export default ContactForm;
--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,11 +1,10 @@
 import React from "react";
+import ContactForm from "../components/contact-form";
+
 const ContactPage = () => 
 	<div>
 		<h2>Contact Me</h2>
-		<p>
-      Looking to get in touch? Shoot me an email at{` `}
-			<a href="cbutera484@gmail.com">cbutera484@gmail.com</a>
-		</p>
+		<ContactForm />
 	</div>
 
 ;
