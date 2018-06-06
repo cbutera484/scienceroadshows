dc6dda8c5b49a8b8a318d746d5aa036932432be3
Chris Butera <cbutera@pch.com>
2018-06-05 21:35:40 -0400

    adding default form validation

--- src/components/contact-form.js
+++ src/components/contact-form.js
@@ -13,27 +13,26 @@ class ContactForm extends React.Component {
 				<div>
 					<label htmlFor="name">Name:</label>
 					<div className="inputWrapper">
-						<input type="text" id="name" name="user_name" />
+						<input type="text" id="name" name="user_name" required />
 						<span className="focus-animation" />
 					</div>
 				</div>
 				<div>
 					<label htmlFor="mail">E-mail:</label>
 					<div className="inputWrapper">
-						<input type="email" id="mail" name="user_mail" />
+						<input type="email" id="mail" name="user_mail" required />
 						<span className="focus-animation" />
 					</div>
 				</div>
 				<div>
 					<label htmlFor="msg">Message:</label>
 					<div className="inputWrapper">
-						<textarea id="msg" name="user_message" />
+						<textarea id="msg" name="user_message" required />
 						<span className="focus-animation" />
 					</div>
 				</div>
-				<div className="button">
-					<button type="submit">Send</button>
-				</div>
+
+				<button type="submit">Send</button>
 			</form>
 		);
 	}
