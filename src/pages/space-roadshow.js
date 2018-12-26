17fce138224fef7eb776dd26fa7da7e8d9632e5e
Chris Butera <cbutera@pch.com>
2018-12-26 14:12:32 -0500

    updating text to match pf

--- src/pages/space-roadshow.js
+++ src/pages/space-roadshow.js
@@ -14,18 +14,18 @@ const SpaceRoadshowPage = () =>
 		</p>
 		<p>
       Looking for a great show or assembly program that’s fun and yet covers
-      your astronomy curriculum? This is it! <strong>The Space Roadshow</strong>
-			{` `}
-      is an in-school field trip to the universe! This live program is presented
-      by science teacher Lee Ognibene, retired director of the Sayville
-      Planetarium and 1998 Suffolk County Science Teacher of the Year.
+      your astronomy curriculum? This is it! The Space Roadshow is an in-school
+      field trip to the universe! This live program is presented by science
+      teacher Lee Ognibene, retired director of the Sayville Planetarium and
+      1998 Suffolk County Science Teacher of the Year.
 		</p>
 		<p>
-			<strong>The Space Roadshow</strong> will transform your auditorium,
-      cafeteria, multi-purpose room, or gym into an exciting astronomy show.
-      Teachers are always amazed how much the students remember even months
-      after seeing this show. Many teachers comment it’s like a cross between
-      Bill Nye, a live Disney show, an IMAX movie, a planetarium show.
+      The Space Roadshow will transform your auditorium, cafeteria,
+      multi-purpose room, or gym into an exciting astronomy show. Teachers are
+      always amazed how much the students remember even months after seeing this
+      show. Many teachers comment my shows are like a cross between Bill Nye, a
+      live Disney show, an IMAX movie, and a planetarium show. They have that
+      “wow” factor.
 			<TextImage src={spaceEarthImage} alt="Earth" />
 		</p>
 		<p>
@@ -35,26 +35,24 @@ const SpaceRoadshowPage = () =>
       system and Milky Way Galaxy, and take field trips to the moon, the
       International Space Station, Meteor Crater in Arizona, Cape Canaveral in
       Florida, an observatory, and a solar eclipse in Hawaii. We also travel to
-      Italy to follow Galileo’s discoveries. There’s even a follow-up art
-      project where your kids can make wonderful chalk drawings of a total solar
-      eclipse. Many live interactive demonstrations and experiments round out
-      the experience.
+      Italy and the moon to try Galileo’s gravity experiment.
 		</p>
 		<p>
-			<strong>The Space Roadshow</strong> is very economical because there is no
-      limit on the number of students who can see the show at the same time.
-      Unlike outside field trips, the Space Roadshow comes to your school so you
-      will have the benefit of saving money on expensive bus transportation and
-      admission fees. Outside field trips can take a whole day but the Space
-      Roadshow is only 1 hour long and provides more learning!. Because the show
-      is live, it can be customized for any grade level, from Kindergarten to
-      9th grade.
+      Your students will also learn about motions of the earth and moon, what
+      causes day and night, what causes the phases of the moon, orbital
+      mechanics, weightlessness, eclipses, telescopes, the solar system, comets,
+      meteors, the sun, our moon, how craters form, galaxies, how big space is,
+      the birth and death of stars, gravity, how rockets work, and our place in
+      space. There’s even a follow-up art project where your kids can make a
+      beautiful chalk drawing of a total solar eclipse. They love it! Many live
+      interactive demonstrations and experiments round out the experience.
 		</p>
 		<p>
-      During the show, I wear a NASA flight suit, just like the astronauts use.
-      There’s lots of humor in the show and after each show, I always hear the
-      kids and teachers say, “That was awesome!” The finale will take your
-      breath away! Expect rave reviews!
+      Because the show is live, I can customized it for any grade level, from
+      Kindergarten to 9th grade. During the show, I wear a NASA flight suit,
+      just like the astronauts. There’s lots of humor in the show and after each
+      show, I always hear the kids and teachers say, “That was awesome!” The
+      finale will take your breath away! Expect rave reviews!
 		</p>
 
 		<Fee />
